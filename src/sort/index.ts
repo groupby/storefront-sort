9d83b27c8e4ae940e75da9ed846fed2f3b607d55
Ben Teichman <ben.l.teichman@gmail.com>
2017-05-18 07:31:23 -0400

    fix sort selection

--- src/sort/index.ts
+++ src/sort/index.ts
@@ -1,33 +1,47 @@
 import { view, Component, Events, Store } from '@storefront/core';
 
-@view('gb-sort', require('./index.html'), require('./index.css'))
+@view('gb-sort', require('./index.html'), require('./index.css'), [
+  { name: 'labels', default: [] }
+])
 class Sort extends Component {
 
+  props: Sort.Props;
   state: Sort.State = {
     sorts: [],
     // TODO make this better by fixin the sort action
-    onSelect: (index) => this.flux.sort(<any>index)
+    onSelect: (index) => this.flux.sort(index)
   };
 
   constructor() {
     super();
-    this.expose('sort');
     this.flux.on(Events.SORTS_UPDATED, this.updateSorts);
   }
 
-  updateSorts = (sorts: Store.Indexed.Selectable<Store.Sort>) =>
-    this.set({
-      sorts: sorts.allIds.map((id) => ({
-        ...sorts.byId[id],
-        // TODO got to have the label in here
-        selected: sorts.selected === id
-      }))
-    })
+  onBeforeMount() {
+    this.state = {
+      ...this.state,
+      sorts: this.selectSorts(this.flux.store.getState().data.sorts)
+    };
+    this.expose('sort');
+  }
+
+  updateSorts = (sorts: Store.SelectableList<Store.Sort>) =>
+    this.set({ sorts: this.selectSorts(sorts) })
+
+  selectSorts(sorts: Store.SelectableList<Store.Sort>) {
+    return sorts.items.map((sort, index) => ({
+      label: this.props.labels[index] || `${sort.field} ${sort.descending ? 'Descending' : 'Ascending'}`,
+      selected: sorts.selected === index
+    }));
+  }
 }
 
 namespace Sort {
+  export interface Props {
+    labels: string[];
+  }
   export interface State {
-    sorts: any[];
+    sorts: Array<{ label: string, selected?: boolean }>;
     onSelect(index: number): void;
   }
 }
