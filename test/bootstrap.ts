6d29392d3b006009a4d661898b4b8d7cb5cce742
Bruce Chen <bruce.chen@groupbyinc.com>
2017-11-27 16:27:56 -0500

    Add tests

--- test/bootstrap.ts
+++ test/bootstrap.ts
@@ -2,5 +2,6 @@ import { bootstrap } from '@storefront/testing';
 import * as chai from 'chai';
 
 bootstrap(chai, __dirname, [
-  '../src/sort/index.html'
+  '../src/sort/index.html',
+  '../src/past-purchases-sort/index.html'
 ]);
