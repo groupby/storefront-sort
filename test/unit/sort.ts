ec2a3ac4017a6e13bb94d9e13ea982128f92ab34
Bruce Chen <bruce.chen@groupbyinc.com>
2017-12-07 15:18:42 -0500

    Update tests

--- test/unit/sort.ts
+++ test/unit/sort.ts
@@ -1,4 +1,4 @@
-import { Events, Selectors } from '@storefront/core';
+import { Events, Selectors, StoreSections} from '@storefront/core';
 import Sort from '../../src/sort';
 import suite from './_suite';
 
@@ -13,7 +13,7 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias })
   describe('constructor()', () => {
     describe('props', () => {
       it('should set initial value', () => {
-        expect(sort.props).to.eql({ labels: [] });
+        expect(sort.props).to.eql({ labels: [], storeSection: StoreSections.DEFAULT });
       });
     });
 
@@ -23,13 +23,24 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias })
       });
 
       describe('onSelect()', () => {
-        it('should call actions.selectSort()', () => {
+        it('should call actions.selectSort() when storeSection is search', () => {
           const selectSort = spy();
+          sort.props.storeSection = StoreSections.SEARCH;
           sort.actions = <any>{ selectSort };
 
           sort.state.onSelect(9);
 
           expect(selectSort).to.be.calledWith(9);
+        });
+
+        it('should call actions.selectPastPurchasesSort() when storeSection is pastPurchases', () => {
+          const selectPastPurchasesSort = spy();
+          sort.props.storeSection = StoreSections.PAST_PURCHASES;
+          sort.actions = <any>{ selectPastPurchasesSort };
+
+          sort.state.onSelect(9);
+
+          expect(selectPastPurchasesSort).to.be.calledWith(9);
         });
       });
     });
@@ -45,14 +56,26 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias })
       expect(updateSorts).to.be.called;
     });
 
-    it('should listen for SORTS_UPDATED', () => {
+    it('should listen for SORTS_UPDATED when storeSection is search', () => {
       const on = spy();
+      sort.props.storeSection = StoreSections.SEARCH;
       sort.updateSorts = () => null;
       sort.flux = <any>{ on };
 
       sort.init();
 
       expect(on).to.be.calledWith(Events.SORTS_UPDATED, sort.updateSorts);
+    });
+
+    it('should listen for PAST_PURCHASE_SORT_UPDATED when storeSection is pastPurchases', () => {
+      const on = spy();
+      sort.props.storeSection = StoreSections.PAST_PURCHASES;
+      sort.updateSorts = () => null;
+      sort.flux = <any>{ on };
+
+      sort.init();
+
+      expect(on).to.be.calledWith(Events.PAST_PURCHASE_SORT_UPDATED, sort.updateSorts);
     });
   });
 
@@ -91,6 +114,28 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias })
         { label: 'x', selected: false },
       ]);
     });
+
+    it('should remap sorts for pastPurchases', () => {
+      const state = { a: 'b' };
+      const getPastPurchasesLabel = sort.getPastPurchasesLabel = spy(() => 'x');
+      const sort1 = { field: 'variant.colour' };
+      const sort2 = { field: 'price' };
+      const sort3 = { field: 'size' };
+      const select = sort.select = spy(() => ({ items: [sort1, sort2, sort3], selected: 1 }));
+      sort.props.storeSection = StoreSections.PAST_PURCHASES;
+
+      const options = sort.extractSorts();
+
+      expect(select).to.be.calledWithExactly(Selectors.pastPurchaseSort);
+      expect(getPastPurchasesLabel).to.be.calledWith(sort1);
+      expect(getPastPurchasesLabel).to.be.calledWith(sort2);
+      expect(getPastPurchasesLabel).to.be.calledWith(sort3);
+      expect(options).to.eql([
+        { label: 'x', selected: false },
+        { label: 'x', selected: true },
+        { label: 'x', selected: false },
+      ]);
+    });
   });
 
   describe('getLabel()', () => {
@@ -105,6 +150,15 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldHaveAlias })
 
       expect(sort.getLabel({ field: 'age', descending: true }, 2)).to.eq('age Descending');
       expect(sort.getLabel({ field: 'age' }, 2)).to.eq('age Ascending');
+    });
+  });
+
+  describe('getPastPurchasesLabel()', () => {
+    it('should return label', () => {
+      const field = 'giraffe';
+      const sortItem = { field };
+
+      expect(sort.getPastPurchasesLabel(sortItem)).to.eq(field);
     });
   });
 });
