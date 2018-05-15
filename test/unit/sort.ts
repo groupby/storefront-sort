eb72dffdef591acfe52e926a765bb191570f2014
Ben Teichman <ben.l.teichman@gmail.com>
2018-05-15 02:33:24 -0400

    update tests

--- test/unit/sort.ts
+++ test/unit/sort.ts
@@ -47,35 +47,32 @@ suite('Sort', ({ expect, spy, stub, itShouldBeConfigurable, itShouldProvideAlias
   });
 
   describe('init()', () => {
-    it('should call updateSorts()', () => {
-      const updateSorts = (sort.updateSorts = spy());
-      sort.flux = <any>{ on: () => null };
-
-      sort.init();
-
-      expect(updateSorts).to.be.called;
-    });
-
     it('should listen for SORTS_UPDATED when storeSection is search', () => {
-      const on = spy();
+      const subscribe = (sort.subscribe = spy());
       sort.props.storeSection = StoreSections.SEARCH;
-      sort.updateSorts = () => null;
-      sort.flux = <any>{ on };
 
       sort.init();
 
-      expect(on).to.be.calledWith(Events.SORTS_UPDATED, sort.updateSorts);
+      expect(subscribe).to.be.calledWith(Events.SORTS_UPDATED, sort.updateSorts);
     });
 
     it('should listen for PAST_PURCHASE_SORT_UPDATED when storeSection is pastPurchases', () => {
-      const on = spy();
+      const subscribe = (sort.subscribe = spy());
       sort.props.storeSection = StoreSections.PAST_PURCHASES;
-      sort.updateSorts = () => null;
-      sort.flux = <any>{ on };
 
       sort.init();
 
-      expect(on).to.be.calledWith(Events.PAST_PURCHASE_SORT_UPDATED, sort.updateSorts);
+      expect(subscribe).to.be.calledWith(Events.PAST_PURCHASE_SORT_UPDATED, sort.updateSorts);
+    });
+  });
+
+  describe('onBeforeMount()', () => {
+    it('should call updateSorts()', () => {
+      const updateSorts = (sort.updateSorts = spy());
+
+      sort.onBeforeMount();
+
+      expect(updateSorts).to.be.called;
     });
   });
 
